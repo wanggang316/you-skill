6666ed995b38e89142a8de32af7c4e1aff979fed
Gump <1989wg@gmail.com>
2026-02-07 19:07:00 +0800

    chore: add vite-plugin-node-polyfills for Node.js polyfills

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [
+    svelte(),
+    nodePolyfills({
+      include: ['buffer'],
+    }),
+  ],
   clearScreen: false,
   server: {
     port: 5173,
